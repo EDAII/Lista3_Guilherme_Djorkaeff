a381cea60b1e65323e99e56743a3d23535074e2c
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2018-09-10 09:10:16 -0300

    Add heapSort

--- lista3_sort.playground/Contents.swift
+++ lista3_sort.playground/Contents.swift
@@ -14,6 +14,50 @@ func generateData(tam: Int) -> [Int] {
     return numbers
 }
 
+func heapSort(_ array: [Int]) -> [Int] {
+    var vet = array
+    
+    let n = vet.count
+    
+    var i = n
+    while(i > -1) {
+        vet = heapfy(vet, n: n, i: i)
+        i -= 1
+    }
+    
+    i = n-1
+    while(i > 0) {
+        vet.swapAt(i, 0)
+        vet = heapfy(vet, n: i, i: 0)
+        i -= 1
+    }
+    
+    return vet
+}
+
+func heapfy(_ array: [Int], n: Int, i: Int) -> [Int] {
+    var vet = array
+    
+    var greater = i
+    var left = 2 * i + 1
+    var right = 2 * i + 2
+
+    if left < n && vet[i] < vet[left] {
+        greater = left
+    }
+    
+    if right < n && vet[greater] < vet[right] {
+        greater = right
+    }
+    
+    if greater != i {
+        vet.swapAt(i, greater)
+        vet = heapfy(vet, n: n, i: greater)
+    }
+    
+    return vet
+}
+
 func shellSort(_ array: [Int]) -> [Int] {
     var vet = array
     
@@ -41,4 +85,5 @@ func shellSort(_ array: [Int]) -> [Int] {
     return vet
 }
 
-print(shellSort(generateData(tam: 1000)))
+//print(shellSort(generateData(tam: 1000)))
+//print(heapSort(generateData(tam: 1000)))
